3b0ddd8d0f56f722cc5f2f505e1a7cee6ff9c419
Faisal Hafeez <fhnaseer@live.com>
2017-08-28 15:07:45 +0200

    adding markers on map,

--- app/src/main/java/de/tum/in/repobruegge/pom17aprilsnow/flightapp/presentation/activities/SeachParkingActivity.java
+++ app/src/main/java/de/tum/in/repobruegge/pom17aprilsnow/flightapp/presentation/activities/SeachParkingActivity.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
@@ -22,16 +25,37 @@ public class SeachParkingActivity extends NavigationItemActivity implements OnMa
     private GoogleMap _map;
     public static final String CURRENT_TRIP_ID = "de.tum.in.repobruegge.pom16aprilsnow.flightapp.currentflight";
     private List<POI> _availableParkings = null;
+    private POI _selectedParking = null;
+    private LatLng _currentPosition = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        _availableParkings = new ArrayList<POI>();
-
+        addParkings();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    private void addParkings() {
+        _availableParkings = new ArrayList<POI>();
+        _currentPosition = new LatLng(48.150474, 11.619199);
+        addParking("Arabellastr 11", 48.150474, 11.619199);
+        addParking("Gotthelfstr 69", 48.141653,11.624478);
+        addParking("Richard-Strauss-Str. 81", 48.147469, 11.615880);
+        addParking("Oberföhringer str. 2", 48.151131, 11.608724);
+        addParking("Elektrastr. 4",         48.153282, 11.621051);
+        addParking("Englschalkinger str. 136", 48.153282, 11.621051);
+        addParking("Denninger str 169", 48.147873, 11.627997);
+    }
+
+    private void addParking(String name, double latitude, double longitude) {
+        POI parking = new POI();
+        parking.setName(name);
+        parking.setLatitude(latitude);
+        parking.setLongitude(longitude);
+        _availableParkings.add(parking);
     }
 
     @Override
@@ -59,7 +83,21 @@ public class SeachParkingActivity extends NavigationItemActivity implements OnMa
     @Override
     public void onMapReady(GoogleMap googleMap) {
         _map = googleMap;
+        _map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean  onMarkerClick(Marker marker) {
+                Toast.makeText(getApplicationContext(), "Clicked a marker with title..." + marker.getTitle(), Toast.LENGTH_SHORT).show();
+                for (POI parking : _availableParkings) {
+                    if (marker.getPosition().latitude == parking.getLatitude() && marker.getPosition().longitude == parking.getLongitude())
+                    {
+                        _selectedParking = parking;
+                    }
+                }
+                return true;
+            }
+        });
         addMarkersToMap();
+        _map.animateCamera(CameraUpdateFactory.newLatLngZoom(_currentPosition,14));
     }
 
     private void addMarkersToMap() {
